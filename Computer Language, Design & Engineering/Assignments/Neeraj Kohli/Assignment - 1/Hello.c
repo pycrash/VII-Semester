94d4ee8d6e665410f741e7cef10dc5aaa15549a9
Ashutosh Jha <contact.pycrash@gmail.com>
2022-01-01 02:39:46 +0530

    Initial Commit

---
+++ Computer Language, Design & Engineering/Assignments/Neeraj Kohli/Assignment - 1/Hello.c
@@ -0,0 +1,6 @@
+    #include <stdio.h> // define the header file  
+    int main(void)   // define the main function  
+    {  
+        printf("Hello World!"); // print the statement.  
+        return 0;
+    }  
